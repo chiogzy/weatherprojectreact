71e91fb4ed34758764f452435660376739441c69
chiogzy <63241649+chiogzy@users.noreply.github.com>
2024-04-03 14:17:44 +0100

    introduced useEffect, editted my code to include temperature in ℃

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
-import WeatherForecastDay from "./WeatherForecastDay";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -29,7 +33,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "a710bd8bd76400c9658ef649d9e81728";
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
